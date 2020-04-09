a0cfd29297597bd3922b01c92c7fff5ada83aa30
sbshams <saad.shams@tum.de>
2020-04-09 20:48:29 +0500

    Using CSV module in python

--- localFile.py
+++ localFile.py
@@ -1,15 +1,47 @@
 import os
 import csv
+#import pandas as pd
 
 script_dir = os.path.dirname(__file__)
 source_file = os.path.join(script_dir, '../covid-19/scripts/data/time-series-19-covid-combined.csv')
 
-with open(source_file, 'r') as file:
+countryName = 'Pakistan'
+localFilePath = 'separated_files/' + countryName + '.csv'
+
+with open(source_file, 'r') as file, open(localFilePath, 'w', newline='') as file_out:
 	reader = csv.reader(file)
+	writer = csv.writer(file_out)
+	
 	i = 0
 	for row in reader:
-		print(row)
+		if (i == 0):
+			writer.writerow(row)
+		if (row[1] == countryName):
+			writer.writerow(row)
 		i = i + 1
-		if(i>3):
-			break
 
+			
+filteredFilePath = 'filtered_data/' + countryName + '.csv'
+
+with open(localFilePath, 'r') as file, open(filteredFilePath, 'w', newline='') as file_out:
+	reader = csv.reader(file)
+	writer = csv.writer(file_out)
+
+	i = 0
+	for row in reader:
+		if (i == 0):
+			writer.writerow(row)
+		elif (row[5] > '0'):
+			writer.writerow(row)
+		i = i + 1
+
+
+
+ 
+### Will be used for sanity checking if file has been written or not
+#
+# check if size of file is 0
+#if os.stat(file_path).st_size == 0:
+ #   print('File is empty')
+#else:
+ #   print('File is not empty')
