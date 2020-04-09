2663f79d1111c040444f0bb945b07965cf36d3a9
sbshams <saad.shams@tum.de>
2020-04-10 00:01:35 +0500

    Shifting to PANDAS for data analysis ease

--- localFile.py
+++ localFile.py
@@ -1,47 +1,25 @@
 import os
-import csv
-#import pandas as pd
+import pandas as pd
+import numpy as np
 
+
+# Country specifics for source data
+countryName = 'Pakistan'
 script_dir = os.path.dirname(__file__)
 source_file = os.path.join(script_dir, '../covid-19/scripts/data/time-series-19-covid-combined.csv')
-
-countryName = 'Pakistan'
 localFilePath = 'separated_files/' + countryName + '.csv'
+localPlotFilePath = 'plot_files/' + countryName + '.csv'
 
-with open(source_file, 'r') as file, open(localFilePath, 'w', newline='') as file_out:
-	reader = csv.reader(file)
-	writer = csv.writer(file_out)
-	
-	i = 0
-	for row in reader:
-		if (i == 0):
-			writer.writerow(row)
-		if (row[1] == countryName):
-			writer.writerow(row)
-		i = i + 1
+# Getting data of specified country
+sourceData = pd.read_csv(source_file)
+countryData = sourceData[sourceData['Country/Region'] == countryName]
+countryData = countryData.dropna(axis = 1)
+countryData.rename(columns = {'Country/Region': 'Country' }, inplace=True)
+countryData.to_csv(localFilePath)
 
-			
-filteredFilePath = 'filtered_data/' + countryName + '.csv'
-
-with open(localFilePath, 'r') as file, open(filteredFilePath, 'w', newline='') as file_out:
-	reader = csv.reader(file)
-	writer = csv.writer(file_out)
-
-	i = 0
-	for row in reader:
-		if (i == 0):
-			writer.writerow(row)
-		elif (row[5] > '0'):
-			writer.writerow(row)
-		i = i + 1
-
-
-
- 
-### Will be used for sanity checking if file has been written or not
-#
-# check if size of file is 0
-#if os.stat(file_path).st_size == 0:
- #   print('File is empty')
-#else:
- #   print('File is not empty')
+# Getting data ready for plot
+countryData = countryData.reset_index( drop = True )
+Index = countryData[countryData['Confirmed'] != 0].index.values
+plotData = countryData.iloc[ Index[0]-1 : Index[-1]+1 ]
+plotData = plotData.reset_index( drop = True )
+plotData.to_csv(localPlotFilePath)
